a518ccc24ca8f14ade980cb126bea9d75a1765e5
Inna Denysiuk <innadenysiuk275@gmail.com>
2024-01-05 13:41:13 +0200

    Added book catalog

--- books.js
+++ books.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const bookCatalog = [
+  {
+    title: 'Clean Code',
+    author: 'Robert C. Martin',
+    category: 'Programming',
+    likes: 25,
+    dislikes: 3,
+  },
+  
+  {
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    category: 'Self-Development',
+    likes: 18,
+    dislikes: 2,
+  },
+  
+  {
+    title: 'Harry Potter and the Philosopher\'s Stone',
+    author: 'J.K. Rowling',
+    category: 'Fantasy',
+    likes: 17,
+    dislikes: 6,
+  },
+  
+  {
+    title: 'Introduction to Algorithms',
+    author: 'Thomas H. Cormen',
+    category: 'Programming',
+    likes: 28,
+    dislikes: 4,
+  },
+  
+  {
+    title: 'The 7 Habits of Highly Effective People',
+    author: 'Stephen R. Covey',
+    category: 'Self-Development',
+    likes: 22,
+    dislikes: 5,
+  },
+];
